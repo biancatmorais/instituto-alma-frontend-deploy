53a61e8e58fa28529d17cd1866ddbc44e05f790c
biancatmorais <bianca.morais@fecap.br>
2025-11-13 03:00:39 -0300

    Removidos automaticamente conflitos do vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-<<<<<<< HEAD
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-=======
 export default defineConfig({
   plugins: [react()],
   base: './',
   build: {
     outDir: 'dist'
   }
->>>>>>> 36459763d99eeb273565214ac8a8f965078ce46d
 })
